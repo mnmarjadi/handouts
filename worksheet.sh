b62f35a6fffd96248c614f58e08deb2f9b5fe282
Meghna Marjadi <mmarjadi@umass.edu>
2018-07-25 09:57:19 -0400

    did some configuration

--- worksheet.sh
+++ worksheet.sh
@@ -1,10 +1,10 @@
 # configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name "Meghna Marjadi"
+git config --global user.email "mmarjadi@umass.edu"
 git commit --no-edit --amend --reset-author
 
 # link your local repository to the origin repository on GitHub
 
-git remote add origin ...
-git push -u origin master
+#git remote add origin ...
+#git push -u origin master
